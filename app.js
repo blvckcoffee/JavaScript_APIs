aa88baee7afa0f6684e2e929d1ad889e3b4d2267
blvckcoffee <joe.johnson.dev@gmail.com>
2021-02-05 15:10:18 -0600

    completed api fetch exercises

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-console.log("Hello World!\n==========\n");
+console.log('Hello World!\n==========\n');
 
 // Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+console.log('EXERCISE 1:\n==========\n');
+
+document.getElementById('submit').addEventListener('click', () => {
+  const img = document.querySelector('img');
+  const input = document.getElementById('searchWord').value;
+
+  fetch(
+    'https://api.giphy.com/v1/gifs/translate?api_key=${api_key}&s=' + input,
+    { mode: 'cors' }
+  )
+    .then((response) => response.json())
+    .then((res) => {
+      img.src = res.data.images.original.url;
+    })
+    .catch((response) => console.log(response));
+});
